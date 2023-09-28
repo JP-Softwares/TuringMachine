2d65e809205b7d6f832b79f02cd15178e73d3f44
Geydson <geydson87@gmail.com>
2023-09-28 20:17:47 -0300

    commit base do projeto

---
+++ src/main/java/com/jp/visao/TelaHome.java
@@ -0,0 +1,24 @@
+package com.jp.visao;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TelaHome implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Run.telaHome = this;
+        
+    }
+}

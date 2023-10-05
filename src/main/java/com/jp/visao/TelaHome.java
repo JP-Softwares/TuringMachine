2f3bf53bfafc7ae886cbfcd21bd4b573049f504d
Geydson <geydson87@gmail.com>
2023-10-05 19:01:10 -0300

    commit

--- src/main/java/com/jp/visao/TelaHome.java
+++ src/main/java/com/jp/visao/TelaHome.java
@@ -12,9 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 
 import java.awt.*;
@@ -27,17 +25,44 @@ import java.util.ResourceBundle;
 public class TelaHome implements Initializable {
 
     @FXML
+    private BorderPane bpAddState;
+
+    @FXML
+    private HBox hboxTeste;
+
+    @FXML
     private AnchorPane paneRoot;
+
+    @FXML
+    void OpenAddState(ActionEvent event) {
+        bpAddState.setVisible(true);
+    }
+
+    @FXML
+    void addState(ActionEvent event) {
+        exitAddState(null);
+    }
+
+    @FXML
+    void exitAddState(ActionEvent event) {
+        bpAddState.setVisible(false);
+
+    }
+
+    private SwitchButton switchButton;
 
     private List<Estado> estados = null;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Run.telaHome = this;
+        switchButton = new SwitchButton();
 
         estados = new LinkedList<>();
 
         estados.add(new Estado("aceita", false, null));
         estados.add(new Estado("rejeita", false, null));
+
+       // hboxTeste.getChildren().add(0, switchButton);
     }
 }
